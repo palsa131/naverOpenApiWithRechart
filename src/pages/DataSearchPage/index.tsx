845a2a4a9e48d6d11ed6a752968b716dc15359ed
Jeong Ho <gkood2@gamil.com>
2022-02-24 04:55:59 +0900

    feat: DataSearchPage 작성

---
+++ src/pages/DataSearchPage/index.tsx
@@ -0,0 +1,11 @@
+import { Wrapper } from './styles';
+import { DataSearchForm } from '../../components/';
+
+const DataSearchPage = () => {
+  return (
+    <Wrapper>
+      <DataSearchForm />
+    </Wrapper>
+  );
+};
+export default DataSearchPage;
---
+++ src/pages/DataSearchPage/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  margin: 24px 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export { Wrapper };
