ad6cf10f87ffd8897b1ebe90b50c48186d9898d3
Jeong Ho <gkood2@gamil.com>
2022-02-26 23:08:55 +0900

    refactor: redux-devtools-extension 적용

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,9 @@
 import { combineReducers, createStore } from 'redux';
 import { searchFormData } from './searchFormData';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({ searchFormData });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
 
 export type RootState = ReturnType<typeof rootReducer>;
