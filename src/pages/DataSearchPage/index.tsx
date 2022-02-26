0941ffa02455855226c1cbec934f7957f8828cff
Jeong Ho <gkood2@gamil.com>
2022-02-26 21:07:13 +0900

    feat: DataSearchPage에 TrendDataChart 컴포넌트 연결

--- src/pages/DataSearchPage/index.tsx
+++ src/pages/DataSearchPage/index.tsx
@@ -1,17 +1,22 @@
 import { Wrapper } from './styles';
-import { DataSearchForm } from '../../components/';
-import { useCallback } from 'react';
+import { DataSearchForm, TrendDataChart } from '../../components/';
+import { useCallback, useState } from 'react';
 import getTrendApi from '../../utils/apis/naverTrendSearch';
 import { SearchFormDataType } from '../../redux/searchFormData/types';
 
 const DataSearchPage = () => {
+  const [responseData, setResponseData] = useState();
+
   const handleOnFinish = useCallback(async (searchFormData: SearchFormDataType) => {
+    console.log(searchFormData);
     const { data } = await getTrendApi(searchFormData);
     console.log(data);
+    setResponseData(data);
   }, []);
   return (
     <Wrapper>
       <DataSearchForm handleOnFinish={handleOnFinish} />
+      <TrendDataChart data={responseData}></TrendDataChart>
     </Wrapper>
   );
 };
